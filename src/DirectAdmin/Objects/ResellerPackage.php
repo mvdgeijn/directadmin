06b5f843d3345eb0a6d4a6810fc865887ce43654
bHosted <marc@vdgeijn.com>
2022-08-22 14:34:47 +0200

    Added getters and setters to ResellerPackage & UserPackage

--- src/DirectAdmin/Objects/ResellerPackage.php
+++ src/DirectAdmin/Objects/ResellerPackage.php
@@ -115,4 +115,526 @@ class ResellerPackage extends BaseObject
         $this->vdomains = $params['vdomains'];
     }
 
+    /**
+     * @return bool
+     */
+    public function isAftp(): bool
+    {
+        return $this->aftp;
+    }
+
+    /**
+     * @param bool $aftp
+     * @return ResellerPackage
+     */
+    public function setAftp(bool $aftp): ResellerPackage
+    {
+        $this->aftp = $aftp;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBandwidth(): int
+    {
+        return $this->bandwidth;
+    }
+
+    /**
+     * @param int $bandwidth
+     * @return ResellerPackage
+     */
+    public function setBandwidth(int $bandwidth): ResellerPackage
+    {
+        $this->bandwidth = $bandwidth;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCatchall(): bool
+    {
+        return $this->catchall;
+    }
+
+    /**
+     * @param bool $catchall
+     * @return ResellerPackage
+     */
+    public function setCatchall(bool $catchall): ResellerPackage
+    {
+        $this->catchall = $catchall;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCgi(): int
+    {
+        return $this->cgi;
+    }
+
+    /**
+     * @param int $cgi
+     * @return ResellerPackage
+     */
+    public function setCgi(int $cgi): ResellerPackage
+    {
+        $this->cgi = $cgi;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCron(): bool
+    {
+        return $this->cron;
+    }
+
+    /**
+     * @param bool $cron
+     * @return ResellerPackage
+     */
+    public function setCron(bool $cron): ResellerPackage
+    {
+        $this->cron = $cron;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDns(): bool
+    {
+        return $this->dns;
+    }
+
+    /**
+     * @param bool $dns
+     * @return ResellerPackage
+     */
+    public function setDns(bool $dns): ResellerPackage
+    {
+        $this->dns = $dns;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDnscontrol(): bool
+    {
+        return $this->dnscontrol;
+    }
+
+    /**
+     * @param bool $dnscontrol
+     * @return ResellerPackage
+     */
+    public function setDnscontrol(bool $dnscontrol): ResellerPackage
+    {
+        $this->dnscontrol = $dnscontrol;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDomainptr(): string
+    {
+        return $this->domainptr;
+    }
+
+    /**
+     * @param string $domainptr
+     * @return ResellerPackage
+     */
+    public function setDomainptr(string $domainptr): ResellerPackage
+    {
+        $this->domainptr = $domainptr;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFtp(): string
+    {
+        return $this->ftp;
+    }
+
+    /**
+     * @param string $ftp
+     * @return ResellerPackage
+     */
+    public function setFtp(string $ftp): ResellerPackage
+    {
+        $this->ftp = $ftp;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInode(): string
+    {
+        return $this->inode;
+    }
+
+    /**
+     * @param string $inode
+     * @return ResellerPackage
+     */
+    public function setInode(string $inode): ResellerPackage
+    {
+        $this->inode = $inode;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIps(): string
+    {
+        return $this->ips;
+    }
+
+    /**
+     * @param string $ips
+     * @return ResellerPackage
+     */
+    public function setIps(string $ips): ResellerPackage
+    {
+        $this->ips = $ips;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLoginKeys(): bool
+    {
+        return $this->login_keys;
+    }
+
+    /**
+     * @param bool $login_keys
+     * @return ResellerPackage
+     */
+    public function setLoginKeys(bool $login_keys): ResellerPackage
+    {
+        $this->login_keys = $login_keys;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMysql(): string
+    {
+        return $this->mysql;
+    }
+
+    /**
+     * @param string $mysql
+     * @return ResellerPackage
+     */
+    public function setMysql(string $mysql): ResellerPackage
+    {
+        $this->mysql = $mysql;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNemailf(): string
+    {
+        return $this->nemailf;
+    }
+
+    /**
+     * @param string $nemailf
+     * @return ResellerPackage
+     */
+    public function setNemailf(string $nemailf): ResellerPackage
+    {
+        $this->nemailf = $nemailf;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNemailml(): string
+    {
+        return $this->nemailml;
+    }
+
+    /**
+     * @param string $nemailml
+     * @return ResellerPackage
+     */
+    public function setNemailml(string $nemailml): ResellerPackage
+    {
+        $this->nemailml = $nemailml;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNemailr(): string
+    {
+        return $this->nemailr;
+    }
+
+    /**
+     * @param string $nemailr
+     * @return ResellerPackage
+     */
+    public function setNemailr(string $nemailr): ResellerPackage
+    {
+        $this->nemailr = $nemailr;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNemails(): string
+    {
+        return $this->nemails;
+    }
+
+    /**
+     * @param string $nemails
+     * @return ResellerPackage
+     */
+    public function setNemails(string $nemails): ResellerPackage
+    {
+        $this->nemails = $nemails;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNsubdomains(): string
+    {
+        return $this->nsubdomains;
+    }
+
+    /**
+     * @param string $nsubdomains
+     * @return ResellerPackage
+     */
+    public function setNsubdomains(string $nsubdomains): ResellerPackage
+    {
+        $this->nsubdomains = $nsubdomains;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNusers(): string
+    {
+        return $this->nusers;
+    }
+
+    /**
+     * @param string $nusers
+     * @return ResellerPackage
+     */
+    public function setNusers(string $nusers): ResellerPackage
+    {
+        $this->nusers = $nusers;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isOversell(): bool
+    {
+        return $this->oversell;
+    }
+
+    /**
+     * @param bool $oversell
+     * @return ResellerPackage
+     */
+    public function setOversell(bool $oversell): ResellerPackage
+    {
+        $this->oversell = $oversell;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPhp(): bool
+    {
+        return $this->php;
+    }
+
+    /**
+     * @param bool $php
+     * @return ResellerPackage
+     */
+    public function setPhp(bool $php): ResellerPackage
+    {
+        $this->php = $php;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isServerip(): bool
+    {
+        return $this->serverip;
+    }
+
+    /**
+     * @param bool $serverip
+     * @return ResellerPackage
+     */
+    public function setServerip(bool $serverip): ResellerPackage
+    {
+        $this->serverip = $serverip;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSpam(): bool
+    {
+        return $this->spam;
+    }
+
+    /**
+     * @param bool $spam
+     * @return ResellerPackage
+     */
+    public function setSpam(bool $spam): ResellerPackage
+    {
+        $this->spam = $spam;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSsh(): bool
+    {
+        return $this->ssh;
+    }
+
+    /**
+     * @param bool $ssh
+     * @return ResellerPackage
+     */
+    public function setSsh(bool $ssh): ResellerPackage
+    {
+        $this->ssh = $ssh;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSsl(): bool
+    {
+        return $this->ssl;
+    }
+
+    /**
+     * @param bool $ssl
+     * @return ResellerPackage
+     */
+    public function setSsl(bool $ssl): ResellerPackage
+    {
+        $this->ssl = $ssl;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSysinfo(): bool
+    {
+        return $this->sysinfo;
+    }
+
+    /**
+     * @param bool $sysinfo
+     * @return ResellerPackage
+     */
+    public function setSysinfo(bool $sysinfo): ResellerPackage
+    {
+        $this->sysinfo = $sysinfo;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isUserssh(): bool
+    {
+        return $this->userssh;
+    }
+
+    /**
+     * @param bool $userssh
+     * @return ResellerPackage
+     */
+    public function setUserssh(bool $userssh): ResellerPackage
+    {
+        $this->userssh = $userssh;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVdomains(): string
+    {
+        return $this->vdomains;
+    }
+
+    /**
+     * @param string $vdomains
+     * @return ResellerPackage
+     */
+    public function setVdomains(string $vdomains): ResellerPackage
+    {
+        $this->vdomains = $vdomains;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuota(): int
+    {
+        return $this->quota;
+    }
+
+    /**
+     * @param int $quota
+     * @return ResellerPackage
+     */
+    public function setQuota(int $quota): ResellerPackage
+    {
+        $this->quota = $quota;
+        return $this;
+    }
+
 }
--- src/DirectAdmin/Objects/UserPackage.php
+++ src/DirectAdmin/Objects/UserPackage.php
@@ -106,4 +106,472 @@ class UserPackage extends BaseObject
         $this->vdomains = $params['vdomains'];
     }
 
+    /**
+     * @return bool
+     */
+    public function isAftp(): bool
+    {
+        return $this->aftp;
+    }
+
+    /**
+     * @param bool $aftp
+     * @return UserPackage
+     */
+    public function setAftp(bool $aftp): UserPackage
+    {
+        $this->aftp = $aftp;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getBandwidth(): int
+    {
+        return $this->bandwidth;
+    }
+
+    /**
+     * @param int $bandwidth
+     * @return UserPackage
+     */
+    public function setBandwidth(int $bandwidth): UserPackage
+    {
+        $this->bandwidth = $bandwidth;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCatchall(): bool
+    {
+        return $this->catchall;
+    }
+
+    /**
+     * @param bool $catchall
+     * @return UserPackage
+     */
+    public function setCatchall(bool $catchall): UserPackage
+    {
+        $this->catchall = $catchall;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCgi(): int
+    {
+        return $this->cgi;
+    }
+
+    /**
+     * @param int $cgi
+     * @return UserPackage
+     */
+    public function setCgi(int $cgi): UserPackage
+    {
+        $this->cgi = $cgi;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isCron(): bool
+    {
+        return $this->cron;
+    }
+
+    /**
+     * @param bool $cron
+     * @return UserPackage
+     */
+    public function setCron(bool $cron): UserPackage
+    {
+        $this->cron = $cron;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDnscontrol(): bool
+    {
+        return $this->dnscontrol;
+    }
+
+    /**
+     * @param bool $dnscontrol
+     * @return UserPackage
+     */
+    public function setDnscontrol(bool $dnscontrol): UserPackage
+    {
+        $this->dnscontrol = $dnscontrol;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDomainptr(): string
+    {
+        return $this->domainptr;
+    }
+
+    /**
+     * @param string $domainptr
+     * @return UserPackage
+     */
+    public function setDomainptr(string $domainptr): UserPackage
+    {
+        $this->domainptr = $domainptr;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFtp(): string
+    {
+        return $this->ftp;
+    }
+
+    /**
+     * @param string $ftp
+     * @return UserPackage
+     */
+    public function setFtp(string $ftp): UserPackage
+    {
+        $this->ftp = $ftp;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInode(): string
+    {
+        return $this->inode;
+    }
+
+    /**
+     * @param string $inode
+     * @return UserPackage
+     */
+    public function setInode(string $inode): UserPackage
+    {
+        $this->inode = $inode;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLanguage(): string
+    {
+        return $this->language;
+    }
+
+    /**
+     * @param string $language
+     * @return UserPackage
+     */
+    public function setLanguage(string $language): UserPackage
+    {
+        $this->language = $language;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLoginKeys(): bool
+    {
+        return $this->login_keys;
+    }
+
+    /**
+     * @param bool $login_keys
+     * @return UserPackage
+     */
+    public function setLoginKeys(bool $login_keys): UserPackage
+    {
+        $this->login_keys = $login_keys;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMysql(): string
+    {
+        return $this->mysql;
+    }
+
+    /**
+     * @param string $mysql
+     * @return UserPackage
+     */
+    public function setMysql(string $mysql): UserPackage
+    {
+        $this->mysql = $mysql;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNemailf(): string
+    {
+        return $this->nemailf;
+    }
+
+    /**
+     * @param string $nemailf
+     * @return UserPackage
+     */
+    public function setNemailf(string $nemailf): UserPackage
+    {
+        $this->nemailf = $nemailf;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNemailml(): string
+    {
+        return $this->nemailml;
+    }
+
+    /**
+     * @param string $nemailml
+     * @return UserPackage
+     */
+    public function setNemailml(string $nemailml): UserPackage
+    {
+        $this->nemailml = $nemailml;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNemailr(): string
+    {
+        return $this->nemailr;
+    }
+
+    /**
+     * @param string $nemailr
+     * @return UserPackage
+     */
+    public function setNemailr(string $nemailr): UserPackage
+    {
+        $this->nemailr = $nemailr;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNemails(): string
+    {
+        return $this->nemails;
+    }
+
+    /**
+     * @param string $nemails
+     * @return UserPackage
+     */
+    public function setNemails(string $nemails): UserPackage
+    {
+        $this->nemails = $nemails;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNsubdomains(): string
+    {
+        return $this->nsubdomains;
+    }
+
+    /**
+     * @param string $nsubdomains
+     * @return UserPackage
+     */
+    public function setNsubdomains(string $nsubdomains): UserPackage
+    {
+        $this->nsubdomains = $nsubdomains;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isPhp(): bool
+    {
+        return $this->php;
+    }
+
+    /**
+     * @param bool $php
+     * @return UserPackage
+     */
+    public function setPhp(bool $php): UserPackage
+    {
+        $this->php = $php;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQuota(): int
+    {
+        return $this->quota;
+    }
+
+    /**
+     * @param int $quota
+     * @return UserPackage
+     */
+    public function setQuota(int $quota): UserPackage
+    {
+        $this->quota = $quota;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSkin(): string
+    {
+        return $this->skin;
+    }
+
+    /**
+     * @param string $skin
+     * @return UserPackage
+     */
+    public function setSkin(string $skin): UserPackage
+    {
+        $this->skin = $skin;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSpam(): bool
+    {
+        return $this->spam;
+    }
+
+    /**
+     * @param bool $spam
+     * @return UserPackage
+     */
+    public function setSpam(bool $spam): UserPackage
+    {
+        $this->spam = $spam;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSsh(): bool
+    {
+        return $this->ssh;
+    }
+
+    /**
+     * @param bool $ssh
+     * @return UserPackage
+     */
+    public function setSsh(bool $ssh): UserPackage
+    {
+        $this->ssh = $ssh;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSsl(): bool
+    {
+        return $this->ssl;
+    }
+
+    /**
+     * @param bool $ssl
+     * @return UserPackage
+     */
+    public function setSsl(bool $ssl): UserPackage
+    {
+        $this->ssl = $ssl;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSuspendAtLimit(): bool
+    {
+        return $this->suspend_at_limit;
+    }
+
+    /**
+     * @param bool $suspend_at_limit
+     * @return UserPackage
+     */
+    public function setSuspendAtLimit(bool $suspend_at_limit): UserPackage
+    {
+        $this->suspend_at_limit = $suspend_at_limit;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isSysinfo(): bool
+    {
+        return $this->sysinfo;
+    }
+
+    /**
+     * @param bool $sysinfo
+     * @return UserPackage
+     */
+    public function setSysinfo(bool $sysinfo): UserPackage
+    {
+        $this->sysinfo = $sysinfo;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getVdomains(): string
+    {
+        return $this->vdomains;
+    }
+
+    /**
+     * @param string $vdomains
+     * @return UserPackage
+     */
+    public function setVdomains(string $vdomains): UserPackage
+    {
+        $this->vdomains = $vdomains;
+        return $this;
+    }
+
 }
