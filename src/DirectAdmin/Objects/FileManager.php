d2a8e28998c794e84e5aa309b169aa93295be275
bHosted <50988060+mvdgeijn@users.noreply.github.com>
2026-05-20 12:22:45 +0200

    Implement deleteFile method in FileManager
    Added a method to delete files from a specified path.

--- src/DirectAdmin/Objects/FileManager.php
+++ src/DirectAdmin/Objects/FileManager.php
@@ -145,6 +145,28 @@ class FileManager extends BaseObject
         return empty($response['error']) || $response['error'] === '0';
     }
 
+
+    /**
+     * Delete one or more files in the specified path.
+     *
+     * @param string       $path  The directory path containing the file(s)
+     * @param string|array $file  The filename or array of filenames to delete
+     * @return bool
+     * @throws GuzzleException
+     */
+    public function deleteFile(string $path, string|array $file): bool
+    {
+        $parameters = [
+            'action'   => 'delete',
+            'path'     => $path,
+            'filename' => (array) $file,
+        ];
+
+        $response = $this->getContext()->invokeApiPost('FILE_MANAGER', $parameters);
+
+        return empty($response['error']) || $response['error'] === '0';
+    }    
+
     /**
      * Invokes a POST command on a domain object.
      *
